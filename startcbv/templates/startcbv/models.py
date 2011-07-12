6b1e78180dc766b6858e21aab31d67836c56f6a5
Audrey M Roy <audreyr@gmail.com>
2011-07-11 17:03:29 -0700

    Basic starter model code.

--- startcbv/templates/startcbv/models.py
+++ startcbv/templates/startcbv/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
-class {{ model_name}}(models.Model):
-    """ TODO - maybe put in created/modified timestamp fields?
-    pass
+class {{ model_name }}(models.Model):
+    """
+        {{ model_name }}
+    """
+    name = models.CharField(_('Name'), max_length=200)
+
+    def __unicode__(self):
+        return u'%s' % self.name
 
