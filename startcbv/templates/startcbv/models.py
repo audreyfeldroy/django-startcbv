14ecd3152794f192e683f1399fcdaf7b8d2cb9ce
Audrey M Roy <audreyr@gmail.com>
2011-07-11 17:34:01 -0700

    Added slug to model.

--- startcbv/templates/startcbv/models.py
+++ startcbv/templates/startcbv/models.py
@@ -5,6 +5,7 @@ class {{ model_name }}(models.Model):
         {{ model_name }} model
     """
     name = models.CharField(_('Name'), max_length=200)
+    slug = models.SlugField()
     pub_date = models.DateField(_('Date Published'))
 
     def __unicode__(self):
