9fccb1508e0d491bf5d2df9773a25c8371214987
Audrey M Roy <audreyr@gmail.com>
2011-07-11 17:12:03 -0700

    Capitalized app name as model name.

--- startcbv/management/commands/startcbv.py
+++ startcbv/management/commands/startcbv.py
@@ -1,4 +1,4 @@
-import os
+import os, string
 
 from django.core.management.base import copy_helper, CommandError, LabelCommand
 from django.utils.importlib import import_module
@@ -6,62 +6,19 @@ from django.utils.importlib import import_module
 from django.template import Context
 from django.template.loader import get_template
 
-def model_generator(model_name):
+def model_generator(app_name, model_name):
     template = get_template('startcbv/models.py')
-    c = Context({'model_name': model_name})
+    c = Context({'model_name': model_name,
+                 'app_name': app_name})
     return template.render(c)
 
 class Command(LabelCommand):
     print "startcbv blah"
-#    help = "Creates a Django app directory structure for the given app name in the current directory."
-#    args = "[appname]"
-#    label = 'application name'
-
-#    requires_model_validation = False
-#    # Can't import settings during this command, because they haven't
-#    # necessarily been created.
-#    can_import_settings = False
 
     def handle_label(self, app_name, directory=None, **options):
         print app_name
-        print model_generator(app_name)
+        print model_generator(app_name, app_name.capitalize())
 
 
 
 
-
-
-
-#        if directory is None:
-#            directory = os.getcwd()
-
-#        # Determine the project_name by using the basename of directory,
-#        # which should be the full path of the project directory (or the
-#        # current directory if no directory was passed).
-#        project_name = os.path.basename(directory)
-#        if app_name == project_name:
-#            raise CommandError("You cannot create an app with the same name"
-#                               " (%r) as your project." % app_name)
-
-#        # Check that the app_name cannot be imported.
-#        try:
-#            import_module(app_name)
-#        except ImportError:
-#            pass
-#        else:
-#            raise CommandError("%r conflicts with the name of an existing Python module and cannot be used as an app name. Please try another name." % app_name)
-
-        
-
-#        copy_helper(self.style, 'app', app_name, directory, project_name)
-
-#class ProjectCommand(Command):
-#    help = ("Creates a Django app directory structure for the given app name"
-#            " in this project's directory.")
-
-#    def __init__(self, project_directory):
-#        super(ProjectCommand, self).__init__()
-#        self.project_directory = project_directory
-
-#    def handle_label(self, app_name, **options):
-#        super(ProjectCommand, self).handle_label(app_name, self.project_directory, **options)
