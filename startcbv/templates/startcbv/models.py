e6d1bd543abde6c13c0869b746bb1ee7bdc46cc2
Audrey M Roy <audreyr@gmail.com>
2011-07-11 17:14:10 -0700

    Minor comment change

--- startcbv/templates/startcbv/models.py
+++ startcbv/templates/startcbv/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class {{ model_name }}(models.Model):
     """
-        {{ model_name }}
+        {{ model_name }} model
     """
     name = models.CharField(_('Name'), max_length=200)
 
